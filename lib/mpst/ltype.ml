9724af4df1aafe795e261283b625b07b490229ff
Francisco Ferreira <fferreira@gmail.com>
2021-04-15 16:49:03 +0100

    added (par) to global types

--- lib/mpst/ltype.ml
+++ lib/mpst/ltype.ml
@@ -477,6 +477,7 @@ let rec project' env unguarded_tv (projected_role : RoleName.t) =
       | _ when is_caller -> gen_invitecreatel next
       | _ when is_participant -> gen_acceptl next
       | _ -> next )
+  | ParG _ -> Err.unimpl "The projection of (par)"
 
 let project projected_role g =
   project'
