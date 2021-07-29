fc41a287d31c679a8d3c2f78a26995c186e20746
Fangyi Zhou <me@fangyi.io>
2021-07-29 11:52:09 +0100

    Downgrade z3 version to avoid glibc version issue

--- utils/install-z3.sh
+++ utils/install-z3.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 
 case "$(uname)" in
   Darwin)
-    Z3_FILENAME=z3-4.8.12-x64-osx-10.15.7
+    Z3_FILENAME=z3-4.8.10-x64-osx-10.15.7
     ;;
   Linux)
     # Assume Debian
-    Z3_FILENAME=z3-4.8.12-x64-glibc-2.31
+    Z3_FILENAME=z3-4.8.10-x64-ubuntu-18.04
     ;;
   *)
     echo "Don't know which z3 to download"
     exit 1
     ;;
 esac
-curl -o z3.zip -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.12/${Z3_FILENAME}.zip
+curl -o z3.zip -L https://github.com/Z3Prover/z3/releases/download/z3-4.8.10/${Z3_FILENAME}.zip
 unzip z3.zip
 rm z3.zip
 mv ${Z3_FILENAME} z3
